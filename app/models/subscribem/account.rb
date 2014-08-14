ace8d777281d92d6b98b97613f681739e03121c7
Mark Hoad <markdhoad@gmail.com>
2014-08-14 14:09:52 +0300

    Finish setting up authentication for accounts

--- app/models/subscribem/account.rb
+++ app/models/subscribem/account.rb
@@ -11,9 +11,20 @@ module Subscribem
 
     belongs_to :owner, :class_name => "Subscribem::User"
     accepts_nested_attributes_for :owner
+    has_many :members, :class_name => "Subscribem::Member"
+    has_many :users, :through => :members
 
     before_validation do
       self.subdomain = subdomain.to_s.downcase
     end
+
+    def self.create_with_owner(params={})
+      account = new(params)
+      if account.save
+        account.users << account.owner
+      end
+      account
+    end
+    
   end
 end
