54c5b119b329cc3ec49ccbcc8aeb130ae789cf73
Mark Hoad <markdhoad@gmail.com>
2014-08-14 13:34:13 +0300

    We are now authenticating users for their own subdomain

--- lib/subscribem/engine.rb
+++ lib/subscribem/engine.rb
@@ -11,9 +11,12 @@ module Subscribem
 
     initializer "subscriber.middleware.warden" do
       Rails.application.config.middleware.use Warden::Manager do |manager|
+        manager.default_strategies :password
+
         manager.serialize_into_session do |user|
           user.id
         end
+        
         manager.serialize_from_session do |id|
           Subscribem::User.find(id)
         end
